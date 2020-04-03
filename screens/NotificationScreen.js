d3a58f69abce5e8fc45bfc032e8adbec6d7e87ce
vishalgaddam873 <vishal18@navgurukul.org>
2020-04-04 02:12:08 +0530

    Donor Interested Notification added to Noyification Screen

---
+++ screens/NotificationScreen.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, FlatList,Text } from 'react-native';
+import { ListItems } from 'react-native-elements';
+import firebase from 'firebase';
+import MyHeader from '../components/MyHeader';
+
+import db from '../config';
+
+export default class NotificationScreen extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userId :  firebase.auth().currentUser.email,
+      allNotifications : []
+    };
+
+    this.notificationRef = null
+  }
+
+  getNotifications=()=>{
+    this.requestRef = db.collection("all_notifications")
+    .where("notification_status", "==", "unread")
+    .where("targeted_user_id",'==',this.state.userId)
+    .onSnapshot((snapshot)=>{
+      var allNotifications =  []
+      snapshot.docs.map((doc) =>{
+        console.log(doc.data());
+        var notification = doc.data()
+        notification["doc_id"] = doc.id
+        allNotification.push(notification)
+      });
+      this.setState({
+          allNotifications : allNotifications
+      });
+    })
+  }
+
+  componentDidMount(){
+    this.getNotifications()
+  }
+
+  componentWillUnmount(){
+    this.notificationRef()
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <View style={{flex:0.1}}>
+          <MyHeader title={"Notifications"} navigation={this.props.navigation}/>
+        </View>
+        <View style={{flex:0.9}}>
+          {
+            this.state.allNotifications.length === 0
+            ?(
+              <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                <Text style={{fontSize:25}}>You have no notifications</Text>
+              </View>
+            )
+            :(
+              <FlatList
+
+              />
+            )
+          }
+        </View>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1
+  }
+})
